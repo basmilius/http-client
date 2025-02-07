3855bf8fc56857a025a99a0322f59a79e4cb1ed6
Bas Milius <basmilius@hotmail.com>
2025-02-07 10:35:37 +0100

    chore: fix generation of types. chore: bump version.

--- build.ts
+++ build.ts
@@ -1,47 +1,4 @@
-import { build, type BunPlugin, file, pathToFileURL, write } from 'bun';
-import { isolatedDeclaration } from 'oxc-transform';
-
-function generateTypesPlugin(): BunPlugin {
-    const visited = new Set<string>();
-
-    return {
-        name: 'oxc-transform-dts',
-
-        setup(builder) {
-            if (!builder.config.root || !builder.config.outdir) {
-                console.warn('osx-transform-dts requires both a root and outdir.');
-                return;
-            }
-
-            const rootPath = pathToFileURL(builder.config.root).pathname;
-            const outPath = pathToFileURL(builder.config.outdir).pathname;
-
-            builder.onStart(() => {
-                visited.clear();
-            });
-
-            builder.onLoad({filter: /\.ts$/}, async args => {
-                if (args.path.startsWith(rootPath) && !visited.has(args.path)) {
-                    visited.add(args.path);
-
-                    const {code} = isolatedDeclaration(
-                        args.path,
-                        await file(args.path).text()
-                    );
-
-                    await write(
-                        args.path
-                            .replace(new RegExp(`^${rootPath}`), outPath)
-                            .replace(/\.ts$/, '.d.ts'),
-                        code
-                    );
-                }
-
-                return undefined;
-            });
-        }
-    };
-}
+import { build } from 'bun';
 
 try {
     await build({
@@ -56,10 +13,7 @@ try {
         target: 'browser',
         external: [
             'luxon',
-            'pinia'
-        ],
-        plugins: [
-            generateTypesPlugin()
+            'vue'
         ]
     });
 } catch (err) {
